02cc277c60592160c09cd15d285e0051e47f6bcc
Emanuel <emahugn2@gmail.com>
2025-03-28 18:10:00 -0600

    impresion de los primeros 50 numeros de Fibonacci

--- RetosProgramacion/src/RetosNumericos.java
+++ RetosProgramacion/src/RetosNumericos.java
@@ -1,3 +1,19 @@
 public class RetosNumericos {
+    public static void main(String[] args) {
+        final int LIMITE = 50;
+        long[] fibonacci = new long[LIMITE];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i < LIMITE; i++) {
+            fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
+        }
+
+        System.out.println("Los primeros " + LIMITE + " números de la secuencia Fibonacci:");
+        for (int i = 0; i < LIMITE; i++) {
+            System.out.println((i+1) + ": " + fibonacci[i]);
+        }
+    }
 
 }
